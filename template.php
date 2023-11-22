58f956b8c78bd884479713c071612a7c58150fd3
Seviko Attalarik <attalarik.handoko@gmail.com>
2023-11-22 16:10:07 +0700

    adding template and booking page

---
+++ template.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en" data-bs-theme="dark">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hotel Booking</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary sticky-top">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="index.php">Hotel Booking</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+            <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+            <?php if (isset($_SESSION['role']) && $_SESSION['role'] == "admin"){ ?>
+                <a class="nav-link" href="add.php">Add</a>
+            <?php } ?>
+        </div>
+        </div>
+        <?php if (isset($_SESSION['username'])){ ?>
+            <a class="nav-link" href="process.php?action=logout">Logout</a>
+        <?php } else { ?>
+            <a class="nav-link" href="login.php">Login</a>
+        <?php } ?>
+    </div>
+    </nav>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+</html>
